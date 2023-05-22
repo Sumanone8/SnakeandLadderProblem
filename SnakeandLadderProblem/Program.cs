c1da6ded8f427525cc2d5b425122d4ea1e5533fb
Sumanone8 <sumanjhabhl@gmail.com>
2023-05-22 23:55:01 +0530

    UC1 implemented

--- SnakeandLadderProblem/Program.cs
+++ SnakeandLadderProblem/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeLadder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int currentPosition = 0;
+            int finalPosition = 100;
+            Console.WriteLine("Start Position is:" + currentPosition);
+        }
+    }
+}
